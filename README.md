23ec685c54024803d146c950e01c7e25616b0682
coder014 <68953464+coder014@users.noreply.github.com>
2023-05-08 22:06:46 +0800

    update: upload a version that passed the in-class tests

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # !!! IMPORTANT !!!
 Due to errors in the testcases, the release of this branch will be delayed until an official explanation is given.
 
+Or you can refer to this may-be-correct branch (**in-class tests passed**).
+
 # OS_buaa
 
 Codes for Operating System course, check them out in the branches.
---
+++ include/syscall.h
@@ -0,0 +1,35 @@
+#ifndef SYSCALL_H
+#define SYSCALL_H
+
+#ifndef __ASSEMBLER__
+
+enum {
+	SYS_putchar,
+	SYS_print_cons,
+	SYS_getenvid,
+	SYS_yield,
+	SYS_env_destroy,
+	SYS_set_tlb_mod_entry,
+	SYS_mem_alloc,
+	SYS_mem_map,
+	SYS_mem_unmap,
+	SYS_exofork,
+	SYS_set_env_status,
+	SYS_set_trapframe,
+	SYS_panic,
+	SYS_ipc_try_send,
+	SYS_ipc_recv,
+	SYS_cgetc,
+	SYS_write_dev,
+	SYS_read_dev,
+	SYS_sem_init,
+	SYS_sem_wait,
+	SYS_sem_post,
+	SYS_sem_getvalue,
+	SYS_sem_getid,
+	MAX_SYSNO,
+};
+
+#endif
+
+#endif
