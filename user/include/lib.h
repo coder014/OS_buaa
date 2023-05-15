80ff974a7bd59dd66ecd3b909c1d8779c0a34b7e
coder014 <68953464+coder014@users.noreply.github.com>
2023-05-15 21:12:01 +0800

    update

--- user/include/lib.h
+++ user/include/lib.h
@@ -72,6 +72,8 @@ int syscall_read_dev(void *, u_int, u_int);
 // ipc.c
 void ipc_send(u_int whom, u_int val, const void *srcva, u_int perm);
 u_int ipc_recv(u_int *whom, void *dstva, u_int *perm);
+u_int get_time(u_int *us);
+void usleep(u_int us);
 
 // wait.c
 void wait(u_int envid);
--- user/lib/ipc.c
+++ user/lib/ipc.c
@@ -3,6 +3,30 @@
 #include <env.h>
 #include <lib.h>
 #include <mmu.h>
+#include <drivers/dev_rtc.h>
+
+u_int get_time(u_int *us) {
+	uint32_t temp;
+	syscall_read_dev(&temp, DEV_RTC_ADDRESS|DEV_RTC_TRIGGER_READ, sizeof(uint32_t));
+	syscall_read_dev(&temp, DEV_RTC_ADDRESS|DEV_RTC_SEC, sizeof(uint32_t));
+	syscall_read_dev(us, DEV_RTC_ADDRESS|DEV_RTC_USEC, sizeof(u_int));
+	return temp;
+}
+void usleep(u_int us) {
+	u_int in_s, in_us;
+	u_int time_s, time_us;
+	in_s = get_time(&in_us);
+	while (1) {
+		time_s = get_time(&time_us);
+		u_int t_s = in_s + us/1000000 + (us%1000000 + in_us)/1000000;
+		u_int t_us = (us%1000000 + in_us)%1000000;
+		if (time_s > t_s || (time_s == t_s && time_us >= t_us)) {
+			return;
+		} else {
+			syscall_yield();
+		}
+	}
+}
 
 // Send val to whom.  This function keeps trying until
 // it succeeds.  It should panic() on any error other than
